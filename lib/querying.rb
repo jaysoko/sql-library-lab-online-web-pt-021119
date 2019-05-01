b54e8c896edce55955794e9a53637bcfc890b989
Jay Sokolowski <jaysoko@gmail.com>
2019-05-01 02:03:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,9 @@ ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto from characters
+ORDER BY LENGTH(motto) DESC
+LIMIT 1;"
 end
 
 

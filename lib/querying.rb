33d30bbef2d8009a881d603974ad5525e286fe91
Jay Sokolowski <jaysoko@gmail.com>
2019-05-01 02:24:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,9 @@ LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, series.title, subgenres.name FROM authors
+JOIN series ON series.author_id = authors.id
+JOIN subgenres ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters

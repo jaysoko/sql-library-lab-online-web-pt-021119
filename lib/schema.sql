7b33b79e96cb36128ef7daee55418290bde1d7c4
Jay Sokolowski <jaysoko@gmail.com>
2019-05-01 01:36:00 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT);
+
+  CREATE TABLE sub-genre (
+  id INTEGER PRIMARY KEY,
+  name TEXT);
+
+  CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT);
+
+  CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER);
+
+  CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT);
+
+  CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT);
